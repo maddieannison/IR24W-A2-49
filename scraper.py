28f5e8fea5fc8d7bd3eaf1ff4a4c528124acf6bf
maddie annison <mannison@uci.edu>
2024-02-11 18:38:08 +0000

    Added functionality to check url similarity and avoid infinite traps

--- scraper.py
+++ scraper.py
@@ -1,8 +1,12 @@
 import re
 from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
+from collections import deque
+from difflib import SequenceMatcher
 
-discovered = set()
+discovered = set() # Stores the links we have found
+crawled = set() # Stores the links we have found AND crawled
+last_ten_links = deque(maxlen=10)
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -32,39 +36,61 @@ def extract_next_links(url, resp):
     links = []
 
     if resp:
-        if resp.status == 200: # avoid any responses other than valid, including 204 No Content
+            # If we have already found this link, return
+            if resp.url in discovered:
+                return links
+
+            discovered.add(resp.url)
+
+            if resp.status == 200: # avoid any responses other than valid, including 204 No Content
+
+                # Check for traps
+                if check_url_similarity(resp.url):
+                    return links
+
+                last_ten_links.append(resp.url)
+        
             
-            if resp.url not in discovered:
-                discovered.add(resp.url)
+                # Extract information from the web response page
+                soup = BeautifulSoup(resp.raw_response.content, 'lxml')
 
-            # Extract information from the web response page
-            soup = BeautifulSoup(resp.raw_response.content, 'lxml')
+                # Extract textual information and check textual information content
+                text = tokenize(soup.get_text().lower())
+                if not text: # tokenize() will return None if there is low textual content - do not crawl
+                    return links 
 
-            # Extract textual information and check textual information content
-            text = tokenize(soup.get_text().lower())
-            if not text: # tokenize() will return None if there is low textual content - do not crawl
-                return links 
+                # If the page has already been scraped, return
+                if resp.url in crawled:
+                    return links
 
-            # Extract anchor information
-            for a_tag in soup.find_all('a'):
-                href = a_tag.get("href")
+                crawled.add(resp.url)
 
-                if href is not None:
-                    href = href.split('#')[0] # Defragment the URL
-                    absolute_url = urljoin(url, href) # Transform relative to absolute URL
-                    links.append(absolute_url)
+                # Extract anchor information
+                for a_tag in soup.find_all('a'):
+                    href = a_tag.get("href")
 
+                    if href is not None:
+                        href = href.split('#')[0] # Defragment the URL
+                        absolute_url = urljoin(url, href) # Transform relative to absolute URL
+                        links.append(absolute_url)
     return links
 
-
-# only crawl urls in the uci ics domain
 def is_valid(url):
     try:
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
-        if not re.match(r"(.*\.ics\.uci\.edu|.*\.cs\.uci\.edu|.*\.informatics\.uci\.edu|.*\.stat\.uci\.edu)$", parsed.hostname.lower()):
+
+        # Only crawl urls in the uci ics domain
+        if not parsed.hostname:
             return False
+        if not re.match(r"(.*\.ics\.uci\.edu|.*\.cs\.uci\.edu|.*\.informatics\.uci\.edu|.*\.stat\.uci\.edu)$", parsed.hostname.lower()):
+                return False
+        
+        # Avoid common traps found during testing
+        if ('page' in url) or ('wp-json' in parsed.path) or ('wgEncodeBroad' in parsed.path) or ("action=download" in parsed.query) or ('ical=' in parsed.query) or ('share=' in parsed.query) or ('id=' in parsed.query) or ('rev=' in parsed.query) or ('precision=' in parsed.query): # or ('stayconnected/stayconnected' in url)
+            return False
+
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
@@ -94,19 +120,17 @@ def is_high_quality_page(tokens):
         return False
     return True
 
-# def get_unique_links(links):
-#     ul = []
-#     count = 0
-
-#     for link in links:
-#         if link not in ul:
-#             count = count + 1
-#             ul.append(link)
-
-#     print ("UNIQUE LINKS: ", count)
-
 def write_report():
     with open("report.txt", "w") as file:
-        file.write("unique urls: " + str(len(discovered)))
+        file.write("unique urls: " + str(len(crawled)))
+
+# Return true for urls with over 90% similarity ratio
+def check_url_similarity(url):
+    for link in last_ten_links:
+        if SequenceMatcher(None, url, link).ratio() >= 0.95:
+            print("SIMILAR LINK DETECTED: ", url, " ", link)
+            return True
+    return False
+
 
         
